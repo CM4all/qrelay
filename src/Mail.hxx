0c3142e654d1c4fc5dcadd6082539348c6378151
Max Kellermann <mk@cm4all.com>
2014-02-19 14:37:33 +0100

    Mail: API documentation

--- src/Mail.hxx
+++ src/Mail.hxx
@@ -9,11 +9,31 @@
 
 #include <vector>
 
+/**
+ * An email received via QMQP.
+ */
 struct QmqpMail {
+    /**
+     * The message.
+     */
     ConstBuffer<char> message;
+
+    /**
+     * The QMQP tail after the message, including the message's
+     * trailing comma and the netstrings describing sender and all
+     * recipients.  This is used to forward the message efficiently
+     * after the message has been edited.
+     */
     ConstBuffer<char> tail;
 
+    /**
+     * The sender email address.
+     */
     ConstBuffer<char> sender;
+
+    /**
+     * The list of recipient email addresses.
+     */
     std::vector<ConstBuffer<char>> recipients;
 
     bool Parse(ConstBuffer<char> input);
