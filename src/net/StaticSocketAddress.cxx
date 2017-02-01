bda432fbd51e9d42360aac392054c8029730e3ed
Max Kellermann <mk@cm4all.com>
2017-02-01 09:47:42 +0100

    net/StaticSocketAddress: replace with BSD-2 library

--- src/net/StaticSocketAddress.cxx
+++ src/net/StaticSocketAddress.cxx
@@ -1,28 +1,73 @@
 /*
- * author: Max Kellermann <mk@cm4all.com>
+ * Copyright (C) 2012-2016 Max Kellermann <max@duempel.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include "StaticSocketAddress.hxx"
 
+#include <algorithm>
+
 #include <assert.h>
 #include <string.h>
-#include <sys/un.h>
+#include <netinet/in.h>
 
-void
-StaticSocketAddress::SetLocal(const char *path)
+StaticSocketAddress &
+StaticSocketAddress::operator=(SocketAddress other)
 {
-    auto &sun = reinterpret_cast<struct sockaddr_un &>(address);
+	size = std::min(other.GetSize(), GetCapacity());
+	memcpy(&address, other.GetAddress(), size);
+	return *this;
+}
 
-    const size_t path_length = strlen(path);
+bool
+StaticSocketAddress::operator==(const StaticSocketAddress &other) const
+{
+	return size == other.size &&
+		memcmp(&address, &other.address, size) == 0;
+}
 
-    assert(path_length < sizeof(sun.sun_path));
+bool
+StaticSocketAddress::SetPort(unsigned port)
+{
+	switch (GetFamily()) {
+	case AF_INET:
+		{
+			auto &a = (struct sockaddr_in &)address;
+			a.sin_port = htons(port);
+			return true;
+		}
 
-    sun.sun_family = AF_LOCAL;
-    memcpy(sun.sun_path, path, path_length + 1);
+	case AF_INET6:
+		{
+			auto &a = (struct sockaddr_in6 &)address;
+			a.sin6_port = htons(port);
+			return true;
+		}
+	}
 
-    if (sun.sun_path[0] == '@')
-        /* abstract socket address */
-        sun.sun_path[0] = 0;
-
-    size = SUN_LEN(&sun);
+	return false;
 }
