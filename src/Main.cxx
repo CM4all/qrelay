550285e5607bfed715a5a185d8b93b90037a1d0c
Max Kellermann <mk@cm4all.com>
2017-02-01 14:03:44 +0100

    Main: catch Run() exceptions

--- src/Main.cxx
+++ src/Main.cxx
@@ -40,17 +40,10 @@ Run(const Config &config)
 
 int
 main(int argc, char **argv)
-{
+try {
     daemon_config.detach = false;
 
-    Config config;
-
-    try {
-        config = ParseCommandLine(argc, argv);
-    } catch (const std::exception &e) {
-        cerr << "error: " << e.what() << endl;
-        return EXIT_FAILURE;
-    }
+    const auto config = ParseCommandLine(argc, argv);
 
     /* timer slack 500ms - we don't care for timer correctness */
     prctl(PR_SET_TIMERSLACK, 500000000, 0, 0, 0);
@@ -61,4 +54,7 @@ main(int argc, char **argv)
 
     cerr << "exiting" << endl;
     return result;
+} catch (const std::exception &e) {
+    cerr << "error: " << e.what() << endl;
+    return EXIT_FAILURE;
 }
