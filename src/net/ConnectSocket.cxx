CM4all/beng-proxy

event/SocketEvent: pass events as unsigned, not short

src/net/ConnectSocket.cxx
@@ -78,7 +78,7 @@ public:
     }
 
 private:
-    void EventCallback(short events);
+    void EventCallback(unsigned events);
 
     /* virtual methods from class Cancellable */
     void Cancel() override;
@@ -106,7 +106,7 @@ ConnectSocket::Cancel()
  */
 
 inline void
-ConnectSocket::EventCallback(short events)
+ConnectSocket::EventCallback(unsigned events)
 {
     if (events & EV_TIMEOUT) {
         handler.OnSocketConnectTimeout();

src/net/ServerSocket.cxx
@@ -153,7 +153,7 @@ ServerSocket::GetLocalAddress() const
 }
 
 void
-ServerSocket::EventCallback(gcc_unused short events)
+ServerSocket::EventCallback(unsigned)
 {
     StaticSocketAddress remote_address;
     auto remote_fd = fd.Accept(remote_address);

src/net/ServerSocket.hxx
@@ -62,7 +62,7 @@ protected:
     virtual void OnAcceptError(std::exception_ptr ep) = 0;
 
 private:
-    void EventCallback(short events);
+    void EventCallback(unsigned events);
 };
 
 #endif

CM4all/qrelay

src/net/ConnectSocket.cxx
@@ -74,7 +74,7 @@ ConnectSocket::Connect(const SocketAddress address)
 }
 
 void
-ConnectSocket::OnEvent(short events)
+ConnectSocket::OnEvent(unsigned events)
 {
     if (events & EV_TIMEOUT) {
         handler.OnSocketConnectTimeout();

src/net/ServerSocket.cxx
@@ -97,7 +97,7 @@ ServerSocket::GetLocalAddress() const
 }
 
 void
-ServerSocket::OnEvent(short)
+ServerSocket::OnEvent(unsigned)
 {
     StaticSocketAddress remote_address;
     auto remote_fd = fd.Accept(remote_address);

src/net/ServerSocket.hxx
@@ -47,7 +47,7 @@ protected:
     virtual void OnAccept(SocketDescriptor &&fd, SocketAddress address) = 0;
 
 private:
-    void OnEvent(short events);
+    void OnEvent(unsigned events);
 };
 
 #endif