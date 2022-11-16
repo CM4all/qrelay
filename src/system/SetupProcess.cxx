782bfd89b234b513fa04f70139d551c7e5b24751
Max Kellermann <mk@cm4all.com>
2022-11-16 14:13:43 +0100

    system/SetupProcess: let systemd set up PR_SET_TIMERSLACK
    Also reduce to 10ms.  Since we have CoarseTimerEvent, most timers have
    a 1s resolution anyway.

--- src/system/SetupProcess.cxx
+++ src/system/SetupProcess.cxx
@@ -41,9 +41,6 @@ SetupProcess()
 {
 	signal(SIGPIPE, SIG_IGN);
 
-	/* timer slack 100ms - we don't care for timer correctness */
-	prctl(PR_SET_TIMERSLACK, 100000000, 0, 0, 0);
-
 	/* reduce glibc's thread cancellation overhead */
 	pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, nullptr);
 }
