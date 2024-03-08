444eeb1f256adaf96c401df5b5f2ea89230db5f0
Max Kellermann <mk@cm4all.com>
2024-03-08 13:14:13 +0100

    Listener: use `using` instead of `typedef`

--- src/Listener.hxx
+++ src/Listener.hxx
@@ -2,16 +2,14 @@
 // Copyright CM4all GmbH
 // author: Max Kellermann <mk@cm4all.com>
 
-#ifndef QMQP_RELAY_SERVER_HXX
-#define QMQP_RELAY_SERVER_HXX
+#pragma once
 
 #include "io/Logger.hxx"
 #include "Connection.hxx"
 #include "event/net/TemplateServerSocket.hxx"
 #include "lua/ValuePtr.hxx"
 
-typedef TemplateServerSocket<QmqpRelayConnection,
+using QmqpRelayListener =
+	TemplateServerSocket<QmqpRelayConnection,
 			     size_t, Lua::ValuePtr,
-			     RootLogger, EventLoop &> QmqpRelayListener;
-
-#endif
+			     RootLogger, EventLoop &>;
